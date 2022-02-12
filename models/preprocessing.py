4a32f3e9f78eb22394d258bc02c3ab3fc93b79ee
Rachel DiMaio <rmdimaio@gmail.com>
2022-02-12 12:17:31 -0500

    added preprocessing function for qa

--- models/preprocessing.py
+++ models/preprocessing.py
@@ -2,6 +2,8 @@ from collections import UserDict
 import glob
 import re
 import pickle
+import json
+import uuid
 
 
 class Document(UserDict):
@@ -112,3 +114,73 @@ def clean_text(text):
     """
     mask = re.compile('\[\*\*.*?\*\*\]', flags=re.S)  # re.S flag means dot matches all chars including \n
     return mask.sub('[MASK]', text)
+
+
+def qa_preprocess(snapshot, json_save_path):
+    """
+    Creates SQuAD formatted training data from a given snapshot. Finds true text in the contents of the reports and saves
+    the SQuAD format QA pairs into a JSON file.
+    
+    Parameters
+    ----------
+    snapshot : dict of Documents
+        Data to be used for training
+    json_save_path: str
+        Path that the JSON file will be saved to
+
+    """
+
+    questions = {'dr':"What is the doctor's name?",
+                'date_taken': "What date was it taken on?",
+                'clinic':"What is the name of the clinic?",
+                'body_part': "What is the body part?",
+                'modality': "What is the imaging modality?"
+    }
+    squad = []
+
+    for key, doc in snapshot.items():
+        relevant_labels = doc.get_labels_to_classify()
+
+        temp = {"title": key, "paragraphs": []}
+        qas = []
+        for q, (label_type, labels) in zip(questions, relevant_labels.items()):
+            
+            if labels is not None:
+                ans_text = labels['true text']
+            else: 
+                break
+            qa = {
+                'answers': [
+                    {
+                    'answer_start': doc['text'].find(ans_text),
+                    'text': ans_text
+                    }
+                ],
+                'question': questions[q],
+                'id':str(uuid.uuid4()),
+            }
+            qas.append(qa)
+        temp['paragraphs'] = [{
+            'context': doc['text'],
+            'qas': qas
+        }]
+        
+        squad.append(temp)
+
+    train_squad = []
+    exclude_keys = []
+    for item in squad:
+        keep = True
+        for qa in item['paragraphs'][0]['qas']:
+            for answers in qa['answers']:
+                if answers['answer_start'] == -1:
+                    keep = False
+        if keep:
+            train_squad.append(item)
+        else: 
+            exclude_keys.append(item['title'])
+
+    train_squad_data = {'data': train_squad}
+
+    with open(json_save_path, 'w') as f:
+        json.dump(train_squad_data, f)
