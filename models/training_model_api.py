12ae852f1966844c24e3383978dbc4ccf1b20378
Trevor Yu <trevor.c.yu@gmail.com>
2022-02-16 23:46:49 -0500

    Update comments in dummy class

--- models/training_model_api.py
+++ models/training_model_api.py
@@ -13,22 +13,23 @@ class TrainingModel(ModelApi):
     def preprocess(self, data_snapshot):
         """Transform data snapshot into training data"""
         # Text:
-        #    Determine how to chunk the text by the MAX_SEQ_LEN, doing best to keep within newlines
-        #    Re-label each chunk with 'text_id' + '_' + i, where i is the enumeration of that chunk
+        #    Determine how to chunk the text by the MAX_SEQ_LEN, doing best to keep within words
+        #    Re-label each chunk with 'text_id' + ':' + str(i), where i is the enumeration of that chunk
         # Labels
         #    Generate training labels for chunk from that document's text
         #    Labels need to be saved with the same key as the text chunks
         # Save all these into a dataframe
         pass
 
-    def train(self, training_data):
+    def train(self, training_data, validation_data=None):
         """Train model on training data and records training metrics"""
         # Transform dataframe into pytorch datasets for training and validation
         # Transform pytorch datasets into dataloaders
         # Initiate model based on parameters set in __init__
         # Iterate over training data, forward pass, compute loss, backprop, evaluate val data
         # Compute metrics every epoch(?) and store them
-        # Compute final training metrics for loss, accuracy, confusion matrix
+        # Compute final training metrics for loss, accuracy, confusion matrix in a dict
+        # Return the metrics dict
         pass
 
     def evaluate(self, test_data_snapshot):
