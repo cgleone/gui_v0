a6867c22f856aad9d77ef850966693cc92ce87c2
Trevor Yu <trevor.c.yu@gmail.com>
2022-02-21 09:39:38 -0500

    Update comments, move epoch training to own function

--- models/ner_model.py
+++ models/ner_model.py
@@ -21,7 +21,7 @@ class NerModel(TrainingModel):
         Parameters
         ----------
         parameters : dict
-            parameters dictionary, model.__getattribute__ will try to look here first
+            parameters dictionary, model.__getattr__ will try to look here if the name doesn't exist in self.__dict__
         """
         super().set_parameters(parameters)
         # Set number of labels
@@ -29,29 +29,59 @@ class NerModel(TrainingModel):
         self.parameters['num_labels'] = len(self.entity_encoding) - 1  # Don't count the 'X' label
 
         # Check if we should use cuda
-        if self.use_cuda:
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if self.use_cuda and torch.cuda.is_available():
+            self.device = 'cuda'
         else:
             self.device = 'cpu'
+        # Setup model bits
         self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_url)
         self.nn = self._init_nn()
         self.optimizer = torch.optim.Adam(params=self.nn.parameters(), lr=self.learning_rate)
 
     def _init_nn(self):
+        """Initialize the PyTorch BERT nn and put onto device"""
         self.nn = BertForTokenClassification.from_pretrained(self.base_model_url, num_labels=self.num_labels)
         self.nn.to(self.device)
         return self.nn
 
     def preprocess(self, data_snapshot, generate_labels=True):
-        """Transform data snapshot into training data"""
+        """Transform data snapshot into a dataframe
+        Returns a dataframe of the text without labels in the case of inference when generate_labels is set to False
+
+        Parameters
+        ----------
+        data_snapshot : dict of Documents
+            data snapshot for training/validation/test data
+        generate_labels : bool, optional
+            Whether to generate training labels (for training and validation), by default True
+
+        Returns
+        -------
+        pd.DataFrame
+            Data transformed into dataframe with keys for ['text', 'id'] and ['label'] if labels are generated
+        """
         if generate_labels:
             df = ner_preprocess(data_snapshot, self.tokenizer, self.entity_labels, self.max_seq_len, self.stride)
         else:
             df = text_split_preprocess(data_snapshot, self.tokenizer, self.max_seq_len, self.stride)
         return df
 
-    def train(self, training_data, validation_data=None):
-        """Train model on training data and records training metrics"""
+    def train(self, training_data, validation_data):
+        """Train the model based on the input parameters
+        Trains self.nn for specified number of epochs, generates training and validation metrics
+
+        Parameters
+        ----------
+        training_data : pd.DataFrame
+            preprocessed dataframe for training data
+        validation_data : pd.DataFrame
+            preprocessed dataframe for validation data
+
+        Returns
+        -------
+        dict
+            self.metrics dictionary
+        """
         # Transform dataframe into pytorch datasets for training and validation
         tr_df = self.preprocess(training_data, generate_labels=True)
         val_df = self.preprocess(validation_data, generate_labels=True)
@@ -60,61 +90,7 @@ class NerModel(TrainingModel):
         val_dataloader = df_to_dataloader(val_df, self.tokenizer, self.tokenizer_params, self.batch_size)
 
         for epoch in range(self.epochs):
-            tr_loss, tr_accuracy = 0, 0
-            nb_tr_examples, nb_tr_steps = 0, 0
-            tr_preds, tr_labels = [], []
-            # Put model in training mode
-            self.nn.train()
-            for idx, batch in enumerate(tr_dataloader):
-                ids = batch['input_ids'].to(self.device, dtype=torch.long)
-                mask = batch['attention_mask'].to(self.device, dtype=torch.long)
-                labels = batch['label'].to(self.device, dtype=torch.long)
-
-                outputs = self.nn(input_ids=ids, attention_mask=mask, labels=labels)
-                loss = outputs.loss
-                tr_logits = outputs.logits
-                tr_loss += loss.item()
-
-                nb_tr_steps += 1
-                nb_tr_examples += labels.size(0)
-
-                if idx % 100 == 0:
-                    loss_step = tr_loss / nb_tr_steps
-                    print(f"Epoch {epoch}: Training loss per 100 training steps: {loss_step}")
-
-                # compute training accuracy
-                flattened_targets = labels.view(-1)  # shape (batch_size * seq_len,)
-                active_logits = tr_logits.view(-1, self.nn.num_labels)  # shape (batch_size * seq_len, num_labels)
-                flattened_predictions = torch.argmax(active_logits, axis=1)  # shape (batch_size * seq_len,)
-
-                # only compute accuracy at active labels
-                active_accuracy = labels.view(-1) != -100  # shape (batch_size, seq_len)
-
-                labels = torch.masked_select(flattened_targets, active_accuracy)
-                predictions = torch.masked_select(flattened_predictions, active_accuracy)
-
-                tr_labels.extend(labels)
-                tr_preds.extend(predictions)
-
-                tmp_tr_accuracy = accuracy_score(labels.cpu().numpy(), predictions.cpu().numpy())
-                tr_accuracy += tmp_tr_accuracy
-
-                # gradient clipping
-                torch.nn.utils.clip_grad_norm_(
-                    parameters=self.nn.parameters(), max_norm=self.max_grad_norm
-                )
-
-                # backward pass
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-
-            # Make training metrics
-            train_metrics = {
-                'epoch': epoch,
-                'loss': tr_loss / nb_tr_steps,
-                'accuracy': tr_accuracy / nb_tr_steps
-            }
+            train_metrics = self._train(tr_dataloader, epoch)
             self.metrics.setdefault('training', []).append(train_metrics)
             # Evaluate validation data
             valid_metrics = self._validate(val_dataloader, epoch)
@@ -123,7 +99,96 @@ class NerModel(TrainingModel):
         # Return the metrics dict
         return self.metrics
 
+    def _train(self, train_dataloader, epoch):
+        """Run one epoch for training and generate metrics
+        Updates weights of self.nn through backpropagation
+        Return metrics with training loss and accuracy
+
+        Parameters
+        ----------
+        train_dataloader : TrainingDataloader
+            torch dataloader with training data
+        epoch : int
+            number for current epoch
+
+        Returns
+        -------
+        dict
+            metrics dict for the training epoch
+        """
+        tr_loss, tr_accuracy = 0, 0
+        nb_tr_examples, nb_tr_steps = 0, 0
+        tr_preds, tr_labels = [], []
+        # Put model in training mode
+        self.nn.train()
+        for idx, batch in enumerate(train_dataloader):
+            ids = batch['input_ids'].to(self.device, dtype=torch.long)
+            mask = batch['attention_mask'].to(self.device, dtype=torch.long)
+            labels = batch['label'].to(self.device, dtype=torch.long)
+
+            outputs = self.nn(input_ids=ids, attention_mask=mask, labels=labels)
+            loss = outputs.loss
+            tr_logits = outputs.logits
+            tr_loss += loss.item()
+
+            nb_tr_steps += 1
+            nb_tr_examples += labels.size(0)
+
+            if idx % 100 == 0:
+                loss_step = tr_loss / nb_tr_steps
+                print(f"Epoch {epoch}: Training loss per 100 training steps: {loss_step}")
+
+            # compute training accuracy
+            flattened_targets = labels.view(-1)  # shape (batch_size * seq_len,)
+            active_logits = tr_logits.view(-1, self.nn.num_labels)  # shape (batch_size * seq_len, num_labels)
+            flattened_predictions = torch.argmax(active_logits, axis=1)  # shape (batch_size * seq_len,)
+
+            # only compute accuracy at active labels
+            active_accuracy = labels.view(-1) != -100  # shape (batch_size, seq_len)
+
+            labels = torch.masked_select(flattened_targets, active_accuracy)
+            predictions = torch.masked_select(flattened_predictions, active_accuracy)
+
+            # tr_labels.extend(labels)
+            # tr_preds.extend(predictions)
+
+            tmp_tr_accuracy = accuracy_score(labels.cpu().numpy(), predictions.cpu().numpy())
+            tr_accuracy += tmp_tr_accuracy
+
+            # gradient clipping
+            torch.nn.utils.clip_grad_norm_(
+                parameters=self.nn.parameters(), max_norm=self.max_grad_norm
+            )
+
+            # backward pass
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        # Make training metrics
+        train_metrics = {
+            'epoch': epoch,
+            'loss': tr_loss / nb_tr_steps,
+            'accuracy': tr_accuracy / nb_tr_steps
+        }
+        return train_metrics
+
     def _validate(self, val_dataloader, epoch):
+        """Run one epoch for validation and generate metrics
+        Return metrics with loss, accuracy, confusion matrix, seq_eval metrics for IOB classes
+
+        Parameters
+        ----------
+        train_dataloader : TrainingDataloader
+            torch dataloader with training data
+        epoch : int
+            number for current epoch
+
+        Returns
+        -------
+        dict
+            metrics dict for the training epoch
+        """
         # put model in evaluation mode
         self.nn.eval()
         eval_loss, eval_accuracy = 0, 0
@@ -170,7 +235,7 @@ class NerModel(TrainingModel):
 
         cm = confusion_matrix(labels, predictions)
         # Get accuracy report of just tags
-        entity_encoding = get_iob_entity_encoding(self.entity_labels)
+        entity_encoding = self.entity_encoding
         labels_to_tags = {v: k for k, v in entity_encoding.items()}
         labels = [labels_to_tags[x] for x in labels]
         predictions = [labels_to_tags[x] for x in predictions]
@@ -181,7 +246,7 @@ class NerModel(TrainingModel):
         metrics = {
             'epoch': epoch,
             'confusion_matrix': cm,
-            'tag_report': report,
+            'iob_tag_report': report,
             'loss': eval_loss,
             'accuracy': eval_accuracy
         }
